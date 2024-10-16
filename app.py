5be66ed72990e2bbae3d518a66997393be64ac8b
Király Zoltán <kiralyzoltan@DE-H3P3353.duna.local>
2024-10-16 12:48:50 +0200

    Initial commit - Flask middleware and Dockerfile

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/get_token', methods=['POST'])
+def get_token():
+    data = request.json  # JSON kérés fogadása
+    form_data = {
+        'grant_type': data.get('grant_type', 'password'),
+        'client_id': data.get('client_id'),
+        'client_secret': data.get('client_secret'),
+        'username': data.get('username'),
+        'password': data.get('password')
+    }
+    
+    # Kérés elküldése ServiceNow felé application/x-www-form-urlencoded formátumban
+    response = requests.post('https://dev227667.service-now.com/oauth_token.do',
+                             data=form_data)
+    
+    return jsonify(response.json())  # Válasz visszaküldése JSON formátumban
+
+if __name__ == '__main__':
+    app.run(debug=True)
